d356a723c97026791fde48012863bccac2e324da
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-05-02 16:24:50 +0200

    chore: update task 2

--- src/02-mutually-exclusive/dual-alert.tsx
+++ src/02-mutually-exclusive/dual-alert.tsx
@@ -9,24 +9,22 @@ Two props might be mutually exclusive (You must pass one of them but not both).
 Goal: Modify the Props type such that it has either a message or a messageId, but not both.
 */
 
-type Variant = "primary" | "secondary" | "success" | "warning";
 
 type Props = {
   message?: string;
   messageId?: string;
-  variant?: Variant;
 };
 
 export const DualAlert: FC<Props> = ({
   message,
   messageId,
-  variant = "primary",
+
 }) => {
   const { formatMessage } = useIntl();
 
   const actualMessage = message ?? formatMessage({ id: messageId });
 
-  return <div className={`alert alert-${variant}`} role="alert">
+  return <div role="alert">
     {actualMessage}
   </div>;
 };
